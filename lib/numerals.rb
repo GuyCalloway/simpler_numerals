e4581b7c914758d1ebb612c97e7c59118eed86fb
GuyCalloway <guyfcalloway@gmail.com>
2019-11-05 21:20:08 +0000

    Refactor: Improved Naming

--- lib/numerals.rb
+++ lib/numerals.rb
@@ -1,22 +1,28 @@
 def numeralize(integer)
     numerals = ""
-    arr = [["I", "V"], ["X", "L"], ["C", "D"], ["M", "(V)"], ["(X)", "(L)"], ["(C)", "(D)"], ["(M)"]] 
-    numArray = integer.to_s.split("")
-    l = numArray.length
-    y = []
-    numArray.each_with_index {|x, i| z = l - i
-          y <<  [x, z-1] }
+    roman_numerals = [["I", "V"], ["X", "L"], ["C", "D"], ["M", "(V)"], ["(X)", "(L)"], ["(C)", "(D)"], ["(M)"]] 
+    number_array = integer.to_s.split("")
+    length = number_array.length
+    num_array_with_position = []
+
+    number_array.each_with_index do |number, i| 
+        position = length - (i + 1)
+        num_array_with_position <<  [number, position] 
+    end
  
-    y.each {|x| if x[0] == "0"
-                elsif ["1", "2", "3"].include? x[0]
-                    numerals << arr[x[1]][0] * x[0].to_i
-              elsif x[0] == "4"
-                    numerals <<  arr[x[1]][0] + arr[x[1]][1]
-              elsif x[0] == "9"
-                numerals <<  arr[x[1]][0] + arr[x[1]+1][0]
-              else
-                z = x[0].to_i - 5
-                numerals << arr[x[1]][1] + arr[x[1]][0] * z
-    end  }
+    num_array_with_position.each do |x|
+      if x[0] == "0"
+      elsif ["1", "2", "3"].include? x[0]
+        numerals << roman_numerals[x[1]][0] * x[0].to_i
+      elsif x[0] == "4"
+        numerals <<  roman_numerals[x[1]][0] + roman_numerals[x[1]][1]
+      elsif x[0] == "9"
+        numerals <<  roman_numerals[x[1]][0] + roman_numerals[x[1]+1][0]
+      else
+        surplus = x[0].to_i - 5
+        numerals << roman_numerals[x[1]][1] + roman_numerals[x[1]][0] * surplus
+      end  
+    end
     return numerals
+
  end
